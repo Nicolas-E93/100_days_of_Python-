4005a9b0624028b5decff909817660b60bc15676
Nicolas Escobar <nicolases301@gmail.com>
2025-02-15 01:48:37 -0500

    brand name generator

--- day1_band_name_generator.py
+++ day1_band_name_generator.py
@@ -1,2 +1,27 @@
-# Print statement
-print('Hello world!')
+# Task 1
+# Print statement in different line (this uses only one print statement)
+print('Hello world!\nHello world!\nHello world!')
+
+# Task 2
+# Concatenating strings
+print("Hello" + " " + "Angela")
+print("Hello " + "Angela")
+print("Hello" + " Angela")
+
+# Task 3
+# input function
+print("Hello "+ input("What is your name?") + "!")
+
+# Task 4
+# Variables and len() function
+print(len(input("What is your name?")))
+# Breaking down the 1-line code above
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+# Final project: Band name generator
+print("Welcome to the brand-new band name generator")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+print("Your band name could be: " + city + " " + pet_name)
